558984daad9e1bdb9bb83a9055e9403c64e1e852
xCUELHO <danilo.justino1@gmail.com>
2021-05-19 11:39:08 -0300

    1° Teste com php

---
+++ Processa.php
@@ -0,0 +1,21 @@
+<HTML>
+<HEAD>
+<TITLE>Resultados</TITLE>
+</HEAD>
+<BODY>
+
+
+
+<?php
+$nome = $_POST["Nome"];
+$Email = $_POST["Email"];
+$Compra = $_POST["Compra"];
+$Outro = $_POST["Outro"];
+$Area = $_POST["area"];
+
+echo "Nome:".$nome."<br>";
+echo "Email:".$Email."<br>";
+echo "Compra:".$Compra."<br>";
+echo "Outro:".$Outro."<br>";
+echo "Mensagem:".$Area."<br>";
+?>
