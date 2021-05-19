9ac10873320b0ba63dfbf0107916d5f966e5b519
xCUELHO <danilo.justino1@gmail.com>
2021-05-19 15:47:54 -0300

    Dia 5 terminado com fracasso de tentativa de email

--- api/index.php
+++ api/index.php
@@ -14,14 +14,6 @@ $Compra = $_POST["Compra"];
 $Outro = $_POST["Outro"];
 $Area = $_POST["area"];
 
-
-$emailx = "danilo.justino1@gmail.com"
-$assunto = "Mensagem proveniente do portfolio"
-$mensagem = "Nome: $nome /n
-Email: $Email/n
-Mensagem: $Area"
-mail ( string $emailx , string $assunto , string $mensagem) : bool
-
 echo "Nome:".$nome."<br>";
 echo "Email:".$Email."<br>";
 echo "Compra:".$Compra."<br>";
